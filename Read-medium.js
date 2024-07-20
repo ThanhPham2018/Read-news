f0a992760157fcb227ead26011dded4336dc9ffd
ThanhPham2018 <45378374+ThanhPham2018@users.noreply.github.com>
2024-07-20 15:06:52 +0700

    Update Read-medium.js

--- Read-medium.js
+++ Read-medium.js
@@ -9,20 +9,37 @@
 // @grant        none
 // ==/UserScript==
 
-(function () {
-  // Lấy URL hiện tại
-  var currentUrl = window.location.toString();
-  var domainsToCheck = ["medium.com", ];
-  var freediumUrl = "https://readmedium.com/";
+if (shouldRedirect(mediumDomains, googleCacheDomain, mediumLinkSelector)) {
+    const url = window.location.href;
+    redirectExternalLink('readmedium.com', 'https://{domain}/{url}', url);
+  }
 
-  // Kiểm tra nếu URL chứa bất kỳ domain nào trong domainsToCheck và không chứa "freedium.cfd"
-  var shouldReplace = domainsToCheck.some(function(domain) {
-    return currentUrl.includes(domain);
-  });
+  function shouldRedirect(mediumDomains, googleCacheDomain, mediumLinkSelector) {
+    return matchDomain(mediumDomains) || 
+           (!matchDomain(googleCacheDomain) && document.querySelector(mediumLinkSelector));
+  }
 
-  if (shouldReplace && !currentUrl.includes("readmedium.com")) {
-    // Thay đổi URL
-    var newUrl = currentUrl.replace("https://medium.com/", freediumUrl);
-    window.location = newUrl;
+  function redirectExternalLink(domain, extUrlTemplate, url) {
+    const extUrl = extUrlTemplate.replace('{domain}', domain).replace('{url}', encodeURIComponent(url));
+    window.location.href = extUrl;
   }
 })();
+
+
+// (function () {
+//   // Lấy URL hiện tại
+//   var currentUrl = window.location.toString();
+//   var domainsToCheck = ["medium.com", ];
+//   var freediumUrl = "https://readmedium.com/";
+
+//   // Kiểm tra nếu URL chứa bất kỳ domain nào trong domainsToCheck và không chứa "freedium.cfd"
+//   var shouldReplace = domainsToCheck.some(function(domain) {
+//     return currentUrl.includes(domain);
+//   });
+
+//   if (shouldReplace && !currentUrl.includes("readmedium.com")) {
+//     // Thay đổi URL
+//     var newUrl = currentUrl.replace("https://medium.com/", freediumUrl);
+//     window.location = newUrl;
+//   }
+// })();
