3c6fc6cfb403bd1502d3f44d4435195703bded15
ThanhPham2018 <45378374+ThanhPham2018@users.noreply.github.com>
2024-07-20 09:51:39 +0700

    Update Read-ft.js

--- Read-ft.js
+++ Read-ft.js
@@ -12,17 +12,17 @@
 (function () {
   // Lấy URL hiện tại
   var currentUrl = window.location.toString();
-  var domainsToCheck = ["ft.com", "medium.com"];
-  var freediumUrl = "https://freedium.cfd/";
+  var domainsToCheck = ["medium.com", ];
+  var freediumUrl = "https://readmedium.com/";
 
   // Kiểm tra nếu URL chứa bất kỳ domain nào trong domainsToCheck và không chứa "freedium.cfd"
   var shouldReplace = domainsToCheck.some(function(domain) {
     return currentUrl.includes(domain);
   });
 
-  if (shouldReplace && !currentUrl.includes("freedium.cfd")) {
+  if (shouldReplace && !currentUrl.includes("readmedium.com")) {
     // Thay đổi URL
-    var newUrl = currentUrl.replace("https://", freediumUrl);
+    var newUrl = currentUrl.replace("https://medium.com/", freediumUrl);
     window.location = newUrl;
   }
 })();
